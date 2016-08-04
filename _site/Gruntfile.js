5439f69e5e4802e504735b1627214f472d695f07
EmilyBrick <emily.brick@buzzfeed.com>
2016-08-04 10:47:51 -0400

    added initial flexbox scss

---
+++ _site/Gruntfile.js
@@ -0,0 +1,78 @@
+// We use Grunt to build our dist files
+// commands are run by the makefile
+var Promise = require('es6-promise').Promise;
+
+global.window = {
+  document: {},
+  Function: function(){}
+};
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
+    sass: {
+      lib: {
+        options: {
+          sourceMap: false
+        },
+        files: [
+          {
+            expand: true,
+            cwd: "_lib/",
+            src: ['solid.scss'],
+            dest: 'dist',
+            ext: '.<%= pkg.version %>.css'
+          }
+        ]
+      }
+    },
+    cssnano: {
+      options: {
+        sourcemap: false
+      },
+      lib: {
+        files: {
+         'dist/solid.<%= pkg.version %>.min.css' : 'dist/solid.<%= pkg.version %>.css'
+        }
+      }
+    },
+    compress: {
+      lib: {
+         options: {
+             archive: 'dist/solid.<%= pkg.version %>.zip',
+             mode: 'zip'
+         },
+         files: [{
+             src: ['**/*'],
+             cwd: '_lib/',
+             expand: true
+         }]
+      }
+    },
+    copy: {
+      latest: {
+        files: [{
+          expand: true,
+          cwd: 'dist/',
+          src: 'solid.<%= pkg.version %>.css',
+          dest: 'dist/',
+          rename: function(dest, src) {
+            src = "solid.latest.css" //always available latest css
+            return dest + src;
+          }
+        }]
+      }
+    }
+  });
+
+grunt.loadNpmTasks('grunt-sass');
+grunt.loadNpmTasks('grunt-cssnano');
+grunt.loadNpmTasks('grunt-contrib-compress');
+grunt.loadNpmTasks('grunt-contrib-copy');
+
+grunt.registerTask('compile_lib', ['sass:lib']);
+grunt.registerTask('minify_lib', ['cssnano:lib']);
+grunt.registerTask('latest_solid', ['copy:latest']);
+grunt.registerTask('zip_lib', ['compress:lib']);
+
+}
---
+++ _site/README.md
@@ -0,0 +1,6 @@
+# Solid
+
+Solid is BuzzFeed's CSS functional CSS styleguide. For more details check out the [Solid Docs](http://solid.buzzfeed.com/).
+
+## Developing Solid
+Any BuzzFeed employee can contribute to Solid. See [Contributing to Solid](https://github.com/buzzfeed/solid/blob/master/CONTRIBUTING.md) and [Developing Solid](https://github.com/buzzfeed/solid/blob/master/DEVELOPING.md).
